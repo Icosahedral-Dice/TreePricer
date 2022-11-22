830895d14046376e48dfc9d0bed1e37156b61038
Mingsen Wang <mingsen.wang.sh@gmail.com>
2022-11-22 14:43:40 -0500

    Initial Commit

---
+++ TreePricer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TreePricer
+//
+//  Created by 王明森 on 11/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
